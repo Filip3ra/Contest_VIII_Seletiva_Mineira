b0d3715ea68ac4512d18b7b7d3d536ba8d40a8e7
Filip3ra <filipimacielrj@gmail.com>
2022-04-27 19:19:40 -0300

    wrong answer

--- D - Safe Packing/main.cpp
+++ D - Safe Packing/main.cpp
@@ -26,19 +26,27 @@ int main(int argc, char const *argv[]){
     int qtd_packages = 0;
     int cont = 0;
     vector<int> myvec;
+    int fibo[50] = {0,1,1,2,3,5,8,13,21,34,55,89,144,233,377,610,987,1597,
+                2584,4181,6765,10946,17711,28657,46368,75025,121393,196418,
+                317811,514229,832040,1346269,2178309,3524578,5702887,9227465,
+                14930352,24157817,39088169,63245986,102334155,165580141,267914296,
+                433494437,701408733,1134903170,1836311903};
 
     cin >> W >> F >> S;
 
-    while ( W && F && S != 0){        
+    while ( W && F && S != 0){
         
         for (int i = 0; i < W; i++){
             cin >> w;
             if(isFibonacci(w))  // se w for fibonacci então é um pacote que será empacotado
                 qtd_packages++;
             else{
-                ++cont;
-                while(!isFibonacci(w+cont)) // se não, calcula quanto de filling vou precisar usar
-                    ++cont;
+                for (int i = 0; i < 50; i++){
+                    if(w<fibo[i]){
+                        cont = fibo[i] - w;
+                        i = 100;
+                    }
+                }                
             }           
             myvec.push_back(cont);
             cont = 0;     
@@ -46,9 +54,9 @@ int main(int argc, char const *argv[]){
         
         sort(myvec.begin(), myvec.end()); // ordeno o vetor de filling 
 
-        for (int i = 1; i < myvec.size(); i++){
+        for (int i = 0; i < myvec.size(); i++){
 
-            F = F - myvec[i];            
+            F = F - myvec[i];
             if( F >= 0)
                 ++qtd_packages; // vou empacotar todos que couberem dentro da qtd de filling
         }
