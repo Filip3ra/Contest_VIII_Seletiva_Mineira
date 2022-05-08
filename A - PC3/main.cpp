a1d194d1456f8ae0ec113234a28aad963e6d69b3
Filip3ra <filipimacielrj@gmail.com>
2022-05-08 16:55:56 -0300

    submite fail, mas arrumei um bug

--- A - PC3/main.cpp
+++ A - PC3/main.cpp
@@ -18,10 +18,11 @@ int main(int argc, char const *argv[]){
 
     cin >> N >> P >> T >> R;
 
+
     vector<int> problems_values;
     int x = 0;
 
-    while(N != P != T != R != 0){
+    while(N != 0 && P != 0 && T != 0 && R != 0){
 
         // Prenche o vetor de pontos
         for(int i=0; i<P; i++){
@@ -74,7 +75,6 @@ int main(int argc, char const *argv[]){
                 table.push_back(row);
 
                 row.clear();
-                score = 0;
             }
             
         }
@@ -83,17 +83,19 @@ int main(int argc, char const *argv[]){
         int winner = table[0][0];
         bool multiple_winners = false;
         vector<int> winners;
-        winners.push_back(table[0][0]); 
 
-        for(int i=1; i<table.size(); i++){
+        for(int i=0; i<table.size(); i++){
             if(table[i][1] > best){
                 best = table[i][1];
                 winner = table[i][0];
                 multiple_winners = false;
                 winners.clear();
+                winners.push_back(table[i][0]);
             }
             else if(table[i][1] == best){
-                multiple_winners = true;
+                if(i>0)
+                    multiple_winners = true;
+                
                 winners.push_back(table[i][0]);
             }
         }
@@ -133,3 +135,50 @@ int main(int argc, char const *argv[]){
 
     return 0;
 }
+
+
+/*
+entrada
+
+1 5 20 6
+3 3 10 30 100
+6 1 16024555 0
+3 2 15895629 4
+3 2 765629 0
+17 4 1120132 0
+3 2 1895629 3
+6 3 9024555 0
+2 5 20 7
+3 10 10 30 100
+6 1 16024555 0
+3 2 15895629 5
+3 2 765629 0
+17 4 1120139 0
+5 4 1895629 0
+6 3 9024555 0
+10 4 2895629 0
+3 5 30 7
+5 5 5 5 5
+6 1 16024555 0
+3 2 15895629 0
+3 2 765629 0
+17 4 1120139 0
+5 4 1895629 0
+6 3 9024555 0
+10 4 2895629 0
+4 5 20 11
+10 20 30 40 50
+8 5 16024555 0
+7 2 15895629 5
+7 2 765629 0
+11 4 1120132 5
+11 4 1120144 0
+8 1 1245455 5
+7 3 1895629 1
+7 1 121545 0
+7 3 121545 0
+8 1 121545 0
+11 2 12121 0
+
+
+*/
